a357ff26aea4fbcc8c121ad7a03114fc69466a8a
Yayan H <168175860+yayannn6@users.noreply.github.com>
2025-09-16 23:57:06 +0700

    Update stock_export_report.py

--- export_stock_report/models/stock_export_report.py
+++ export_stock_report/models/stock_export_report.py
@@ -15,12 +15,14 @@ class ReportExportStock(models.AbstractModel):
             ('picking_type_code', '=', 'outgoing'),
             ('state', 'in', ['waiting', 'confirmed', 'assigned']),
             ('picking_type_id.warehouse_id', 'in', wizard.warehouse_ids.ids or self.env['stock.warehouse'].search([]).ids),
+            ('scheduled_date', '>=', wizard.start_date), 
+            ('scheduled_date', '<=', wizard.end_date),  
         ])
 
         results = defaultdict(
             lambda: defaultdict(
                 lambda: defaultdict(
-                    lambda: defaultdict(lambda: {"qty": 0, "uom": ""})
+                    lambda: defaultdict(lambda: {"box": 0, "cont": 0, "variant_values": []})
                 )
             )
         )
@@ -36,12 +38,18 @@ class ReportExportStock(models.AbstractModel):
             for ml in picking.move_line_ids:
                 prod = ml.product_id.name
                 products.add(prod)
+                variant_values = ml.product_id.product_template_variant_value_ids.mapped('name')
 
                 qty = ml.quantity
-                uom = ml.product_uom_id.name or ml.product_id.uom_id.name
 
-                results[salesperson][customer][prod][wh_name]["qty"] += qty
-                results[salesperson][customer][prod][wh_name]["uom"] = uom
+                # Menghitung box dan cont sesuai dengan aturan yang diberikan
+                box = qty  # Box = quantity
+                cont = qty / 0.002  # Cont = quantity / 0.002
+
+                # Menyimpan box dan cont ke dalam results
+                results[salesperson][customer][prod][wh_name]["box"] += box
+                results[salesperson][customer][prod][wh_name]["cont"] += cont
+                results[salesperson][customer][prod][wh_name]["variant_values"] = variant_values
 
         return {
             "doc_ids": docids,
